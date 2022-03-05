f6dc5f0781798dd60645ee93a8d391d51bc16a7e
Julio S <Jso.programing@gmail.com>
2022-03-04 21:40:44 -0500

    Add function to retrive coordinates from a city.
    Added function to take a city name and convert to coordinates to pass into openweather onecall api. Added function to take cordinates and retirve data from openweather api.

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
+import WEATHER_API_KEY from "./apiKey.js";
+
 const unitTemperature = document.querySelector(".toggle-switch");
 const searchIcon = document.querySelector(".search-icon");
 const searchElement = document.querySelector(".search-background");
 
-//Flase is farenheight true is celcius
-// console.log(unitTemperature.checked);
-// console.log(searchIcon);
-console.log(searchElement);
+const apiUrl = "https://api.openweathermap.org/data/2.5/";
+const apiGeocodingUrl = "http://api.openweathermap.org/geo/1.0/direct?q=";
 
-const closeSearch = () => {
-  searchElement.style.display = "none";
+const cityToCordinates = async (city) => {
+  try {
+    let url = `${apiGeocodingUrl}` + `${city}&limit=2&appid=${WEATHER_API_KEY}`;
+    const response = await fetch(url);
+    const geoCord = await response.json();
+    oneCallAPI(geoCord);
+  } catch (error) {
+    console.log(error);
+  }
 };
-
-searchIcon.addEventListener("click", () => {
-  searchElement.style.display = "block";
-});
 
 const unitVerify = (unitTemperature) => {
   let unit = "C";
@@ -25,4 +28,26 @@ const unitVerify = (unitTemperature) => {
   return unit;
 };
 
-unitVerify(unitTemperature);
+const oneCallAPI = async (cordArray) => {
+  try {
+    const url = `${apiUrl}` + `onecall?lat=${cordArray[0].lat}&lon=${cordArray[0].lon}&exclude=minutely,alerts&units=imperial&appid=${WEATHER_API_KEY}`;
+    const response = await fetch(url);
+    const weatherData = await response.json();
+    console.log(weatherData);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const currentWeather = () => {};
+
+const closeSearch = () => {
+  searchElement.style.display = "none";
+};
+
+searchIcon.addEventListener("click", () => {
+  searchElement.style.display = "block";
+});
+
+// let cityName = "Atlanta";
+// cityToCordinates(cityName.toLowerCase());
