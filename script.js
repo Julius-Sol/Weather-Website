c1c395308806df32fb50b9d4d4d9015310dd9371
Julio S <Jso.programing@gmail.com>
2022-03-05 18:53:26 -0500

    Add function to convert unix time to legible date format

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ const unitTemperature = document.querySelector(".toggle-switch");
 const searchIcon = document.querySelector(".search-icon");
 const searchElement = document.querySelector(".search-background");
 
+// Query Selectors for current weather
+const currentWeatherDate = document.querySelector(".main-weather-date");
+
 const apiUrl = "https://api.openweathermap.org/data/2.5/";
 const apiGeocodingUrl = "http://api.openweathermap.org/geo/1.0/direct?q=";
 
@@ -18,16 +21,6 @@ const cityToCordinates = async (city) => {
   }
 };
 
-const unitVerify = (unitTemperature) => {
-  let unit = "C";
-  if (unitTemperature.checked === true) {
-    unit = "C";
-  } else {
-    unit = "F";
-  }
-  return unit;
-};
-
 const oneCallAPI = async (cordArray) => {
   try {
     const url = `${apiUrl}` + `onecall?lat=${cordArray[0].lat}&lon=${cordArray[0].lon}&exclude=minutely,alerts&units=imperial&appid=${WEATHER_API_KEY}`;
@@ -39,7 +32,28 @@ const oneCallAPI = async (cordArray) => {
   }
 };
 
-const currentWeather = () => {};
+const currentWeather = (currentData) => {
+  const date = getDateTitle(1646524179);
+  currentWeatherDate.textContent = date;
+};
+
+//Function that take a unix time and converts it to the format of [Day of the week], [Month(text)] [day]
+const getDateTitle = (unixTime) => {
+  let date = new Date(unixTime * 1000).toDateString();
+  let dateStart = date.substring(0, 3);
+  date = dateStart.concat("," + date.substring(3, 10));
+  return date;
+};
+
+const unitVerify = (unitTemperature) => {
+  let unit = "C";
+  if (unitTemperature.checked === true) {
+    unit = "C";
+  } else {
+    unit = "F";
+  }
+  return unit;
+};
 
 const closeSearch = () => {
   searchElement.style.display = "none";
@@ -48,6 +62,8 @@ const closeSearch = () => {
 searchIcon.addEventListener("click", () => {
   searchElement.style.display = "block";
 });
+
+currentWeather();
 
 // let cityName = "Atlanta";
 // cityToCordinates(cityName.toLowerCase());
